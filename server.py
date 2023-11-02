2c6b4679f52bfe1032b08dec4bbb6a72a14668b4
mmt-1234 <pakkpakk0207@gmail.com>
2023-11-02 20:40:56 +0900

    version1.1

--- server.py
+++ server.py
@@ -11,25 +11,25 @@
 from io import BytesIO
 from bardapi import Bard
 from flask import Flask, render_template, request, send_file
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-# class Upload(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    filname = db.Column(db.String(50))
-#    data = db.Column(db.LargeBinary)
+class Upload(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   filname = db.Column(db.String(50))
+   data = db.Column(db.LargeBinary)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
    if request.method == 'POST':
       file = request.files['file']
 
-      # upload = Upload(filename = file.filename, data=file.read())
-      # db.session.add(upload)
-      # db.session.commit()
+      upload = Upload(filename = file.filename, data=file.read())
+      db.session.add(upload)
+      db.session.commit()
 
       return f'Uploaded: {file.filename}'
    return render_template('input.html')
